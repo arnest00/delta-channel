88db99147e960289faac843aa24ea2ae4af03a15
Osmond <osmondarnesto@gmail.com>
2021-03-10 14:27:24 -0500

    Bootstrap frontend with create-react-app

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 
-const express = require('express');
+const express = require('express'),
+      mongoose = require('mongoose');
 
 const app = express();
 
 // ====== Set up .env variables
+const DB = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
+
+// ====== Connect to MongoDB
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('Connected to database!'))
+  .catch(err => console.log(err));
 
 // ====== Routes
 app.get('/', (req, res) => {
