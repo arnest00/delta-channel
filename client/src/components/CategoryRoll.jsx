74b825945a4e45d33e0acfc4901a8e060fdbc98a
Osmond <osmondarnesto@gmail.com>
2021-03-18 14:44:34 -0400

    Reinstall react-router-dom in correct dir, finalize inaugural boards

--- client/src/components/CategoryRoll.jsx
+++ client/src/components/CategoryRoll.jsx
@@ -1,42 +1,40 @@
 import React from 'react';
-import { Link, BrowserRouter as Router } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-const CategoryRoll = () => {
+const CategoryRoll = props => {
   return ( 
-    <Router>
-      <article>
-        <h2>categories</h2>
-          <section>
-            <h3>
-              <Link to='/pe'>passiveEntertainment</Link>
-            </h3>
-            <span className='description'>Movies and television</span>
-          </section>
+    <article>
+      <h2>{props.user}'s categories</h2>
+        <section>
+          <h3>
+            <Link to='/pe'>passiveEntertainment</Link>
+          </h3>
+          <span className='description'>Movies and television</span>
+        </section>
 
-        <div>
-          <section>
-            <h3>
-              <Link to='/st'>smallTalk</Link>
-            </h3>
-            <span className='description'><i>n.</i> <b>1.</b> polite conversation about unimportant things</span>
-          </section>
+      <div>
+        <section>
+          <h3>
+            <Link to='/st'>smallTalk</Link>
+          </h3>
+          <span className='description'><i>n.</i> <b>1.</b> polite conversation about unimportant things</span>
+        </section>
 
-          <section>
-            <h3>
-              <Link to='/tt'>tableTop</Link>
-            </h3>
-            <span className='description'>Pen and paper RPGs</span>
-          </section>
-          
-          <section>
-            <h3>
-              <Link to='/vg'>videoGames</Link>
-            </h3>
-            <span className='description'>Console, PC, retro</span>
-          </section>
-        </div>
-      </article>
-    </Router>
+        <section>
+          <h3>
+            <Link to='/tt'>tableTop</Link>
+          </h3>
+          <span className='description'>Pen and paper RPGs</span>
+        </section>
+        
+        <section>
+          <h3>
+            <Link to='/vg'>videoGames</Link>
+          </h3>
+          <span className='description'>Console, PC, retro</span>
+        </section>
+      </div>
+    </article>
   );
 };
 
--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -1,11 +1,47 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import CategoryRoll from './CategoryRoll';
 
 const Container = () => {
   return ( 
     <main>
-      <CategoryRoll />
+      <Switch>
+        <Route path='/pe'>
+          <PE />
+        </Route>
+        <Route path='/st'>
+          <ST />
+        </Route>
+        <Route path='/tt'>
+          <TT />
+        </Route>
+        <Route path='/vg'>
+          <VG />
+        </Route>
+        <Route path='/'>
+          <CategoryRoll 
+            user={'Osmond'}
+          />
+        </Route>
+      </Switch>
     </main>
   );
+};
+
+function PE() {
+  return <h2>passiveEntertainment</h2>;
+};
+
+function ST() {
+  return <h2>smallTalk</h2>;
+};
+
+function TT() {
+  return <h2>tableTop</h2>;
+};
+
+function VG() {
+  return <h2>videoGames</h2>;
 };
  
 export default Container;
